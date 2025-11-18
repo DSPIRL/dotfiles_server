f26434e995575c3cef6fc5266d0fd3f26ce997c1
DSPIRL <71217268+DSPIRL@users.noreply.github.com>
2025-11-18 09:02:59 -0500

    added wezterm to install

--- install_scripts/ubuntu_install.sh
+++ install_scripts/ubuntu_install.sh
@@ -35,6 +35,14 @@ if [[ "$operatingSystem" == "Ubuntu" ]]; then
     echo "deb [trusted=yes] https://apt.fury.io/rsteube/ /" | sudo tee /etc/apt/sources.list.d/fury.list
     sudo apt-get update && sudo apt-get install carapace-bin
 
+
+    # Wezterm install
+    curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+    sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+    sudo apt update
+    sudo apt install wezterm-nightly
+
     # OhMyPosh Setup
     curl -s https://ohmyposh.dev/install.sh | bash -s
 
